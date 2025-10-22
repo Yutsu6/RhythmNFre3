0a59b96c83b7963baf4113a5d9a8cde4671132b6
yutsu <794381475@qq.com>
2025-10-22 17:27:06 +0800

    Revert "重传了一下，嵌套部分"
    This reverts commit 9422779a50d1da2d40a972c595f49f7d737820eb.

--- Assets/Scripts/Note/NoteData.cs
+++ Assets/Scripts/Note/NoteData.cs
@@ -4,30 +4,25 @@ using UnityEngine;
 [System.Serializable]
 public class NoteData
 {
-    // 现有字段...
-    public int rowId;
-    public float position;
-    public string type;
-    public float length;
+    public int rowId;   //行
+    public float position;  //在该行的起始位置
+    public string type;     //类型
+    public float length;    //长度
+
     public bool isJudged = false;
     public string judgmentResult = "";
     public GameObject noteObject;
     public float triggerTime;
-    public int indentLevel = 0;
+
+    public int indentLevel = 0;                    // 缩进级别
+
     public bool hasEnteredJudgmentQueue = false;
 
-    // 循环符号相关字段
-    public bool hasLoopSymbol = false;
-    public int[] loopCodes;
-    public string loopRawData = "";
-    public bool loopSymbolTriggered = false;
+    // 结构符号相关字段 - 移除了只读限制
+    public bool hasLoopSymbol = false;          // 是否有循环符号
+    public int[] loopCodes;     // 循环码数组
+    public string loopRawData = "";             // 原始循环数据（用于调试）
 
-    // 新增：if符号相关字段
-    public bool hasIfSymbol = false;
-    public int[] ifConditionCodes;
-    public string ifRawData = "";
-    public bool ifSymbolTriggered = false;
-
-    // 结构符号列表
+    // 新增：结构符号列表，支持多种符号
     public List<StructureSymbol> structureSymbols = new List<StructureSymbol>();
 }
